e28bfa5a349a912ad22d167ea184cc2eeeee90fd
yiyurain <1641890628@qq.com>
2021-06-06 12:43:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chenyi
+ * @date 2021-06-06-12:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
